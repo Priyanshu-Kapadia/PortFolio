53cd6ae0c03eb931f3624dd8a8ddb8890b8cac78
PriyanshuKapadia <priyanshu.kapadia@gmail.com>
2022-07-24 21:38:13 +0530

    About Changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./about.css";
-import ME from "../../assets/me-about.jpg";
+import ME from "../../assets/me-1.png";
 import { FaAward } from "react-icons/fa";
 import { FiUsers } from "react-icons/fi";
 import { VscFolderActive } from "react-icons/vsc";
